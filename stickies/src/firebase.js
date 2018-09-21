01b4c8d627a05ad07eaafb191e03ab6d555262d0
Dnguy104 <dnguy104@ucr.edu>
2018-09-21 15:23:08 -0700

    started new project

---
+++ stickies/src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase'
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDi8nZTNaUnnBtb-qcNHk-nGjo4bJicUCU",
+    authDomain: "stickies-49973.firebaseapp.com",
+    databaseURL: "https://stickies-49973.firebaseio.com",
+    projectId: "stickies-49973",
+    storageBucket: "stickies-49973.appspot.com",
+    messagingSenderId: "761836196228"
+};
+
+firebase.initializeApp(config);
+export default firebase;
