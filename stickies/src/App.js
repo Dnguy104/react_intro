24c5471cd35ee7e3173de585bcf284c606b0b8bf
Dnguy104 <dnguy104@ucr.edu>
2018-10-08 16:52:06 -0700

    fixed database access problem

--- stickies/src/App.js
+++ stickies/src/App.js
@@ -13,56 +13,23 @@ class App extends Component {
     }
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.login = this.login.bind(this); 
+    this.login = this.login.bind(this);
     this.logout = this.logout.bind(this);
   }
-  render() {
-    return (
-      <div className='app'>
-        <header>
-            <div className="wrapper">
-              <h1>Fun Food Friends</h1>
-              {this.state.user ?
-                <button onClick={this.logout}>Log Out</button>                
-                :
-                <button onClick={this.login}>Log In</button>              
-              }
-            </div>
-        </header>
-        <div className='container'>
-          <section className="add-item">
-            <form onSubmit={this.handleSubmit}>
-              <input type="text" name="username" placeholder="What's your name?" onChange={this.handleChange} value={this.state.username} />
-              <input type="text" name="currentItem" placeholder="What are you bringing?" onChange={this.handleChange} value={this.state.currentItem} />
-              <button>Add Item</button>
-            </form>
-          </section>
-          <section className='display-item'>
-            <div className="wrapper">
-              <ul>
-                {this.state.items.map((item) => {
-                  return (
-                    <li key={item.id}>
-                      <h3>{item.title}</h3>
-                      <p>brought by: {item.user}</p>
-                      <button onClick={() => this.removeItem(item.id)}>Remove Item</button>
-                    </li>
-                  )
-                })}
-              </ul>
-            </div>
-          </section>
-        </div>
-      </div>
-    );
-  }
-  
   handleChange(e) {
     this.setState({
       [e.target.name]: e.target.value
     });
   }
-  
+  login() {
+    auth.signInWithPopup(provider) 
+      .then((result) => {
+        const user = result.user;
+        this.setState({
+          user
+        });
+      });
+  }
   logout() {
     auth.signOut()
       .then(() => {
@@ -71,57 +38,103 @@ class App extends Component {
         });
       });
   }
-  
-  login() {
-    auth.signInWithPopup(provider) 
-      .then((result) => {
-        const user = result.user;
-        this.setState({
-          user: user
-        });
-      });
-  }
-  
   handleSubmit(e) {
     e.preventDefault();
     const itemsRef = firebase.database().ref('items');
     const item = {
       title: this.state.currentItem,
-      user: this.state.username
+      user: this.state.user.displayName || this.state.user.email
     }
     itemsRef.push(item);
     this.setState({
       currentItem: '',
-      username: ''
+      username: '',
     });
   }
-  
   componentDidMount() {
-    const itemsRef = firebase.database().ref('items');
-    itemsRef.on('value', (snapshot) => {
-      let items = snapshot.val();
-      let newState = [];
-      for (let item in items) {
-        newState.push({
-          id: item,
-          title: items[item].title,
-          user: items[item].user
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        const itemsRef = firebase.database().ref('items');
+        itemsRef.on('value', (snapshot) => {
+          let items = snapshot.val();
+          let newState = [];
+          for (let item in items) {
+            newState.push({
+              id: item,
+              title: items[item].title,
+              user: items[item].user
+            });
+          }
+          this.setState({
+            items: newState,
+            user: user
+          });
+
         });
-      }
-      this.setState({
-        items: newState
-      });
+      } 
     });
+    
   }
-  
   removeItem(itemId) {
     const itemRef = firebase.database().ref(`/items/${itemId}`);
     itemRef.remove();
+    console.log(itemId);
   }
-  
-  
+  render() {
+    return (
+      <div className='app'>
+        <header>
+            <div className="wrapper">
+              <h1>PotLuck Notes</h1>
+              {this.state.user ?
+                <button onClick={this.logout}>Logout</button>                
+              :
+                <button onClick={this.login}>Log In</button>              
+              }
+            </div>
+        </header>
+          <div>
+            <div className='container'>
+              <section className='add-item'>
+                    <form onSubmit={this.handleSubmit}>
+                      <input type="text" name="username" placeholder="What's your name?" onChange={this.handleChange} value={this.state.user ? this.state.user.displayName : ''} />
+                      <input type="text" name="currentItem" placeholder="What are you bringing?" onChange={this.handleChange} value={this.state.currentItem} />
+                      {this.state.user ?
+                        <button>Add Item</button>
+                        :
+                        null
+                      }
+                    </form>
+              </section>
+              
+              {this.state.user ?
+              
+                <section className='display-item'>
+                    <div className="wrapper">
+                      <ul>
+                        {this.state.items.map((item) => {
+                          return (
+                            <li key={item.id}>
+                              <h3>{item.title}</h3>
+                              <p>brought by: {item.user}
+                                {item.user === this.state.user.displayName || item.user === this.state.user.email ?
+                                  <button onClick={() => this.removeItem(item.id)}>Remove Item</button> : null}
+                              </p>
+                            </li>
+                          )
+                        })}
+                      </ul>
+                    </div>
+                </section>
+                
+                : 
+                  <p>You must be logged in to see the potluck list and submit to it.</p>
+              }
+            </div>
+          </div>
+        
+      </div>
+    );
+  }
 }
-
-
-
 export default App;
